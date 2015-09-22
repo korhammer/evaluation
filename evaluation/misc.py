18ba300f1791b98da8039f0939542c1b980951e1
Daniela Korhammer <korhammd@in.tum.de>
2015-09-22 13:55:09 +0200

    based_floor and based_ceil now round to multiples of the base without casting to int, such that they work for probabilities too

--- evaluation/misc.py
+++ evaluation/misc.py
@@ -3,18 +3,18 @@ __author__ = 'korhammer'
 import numpy as np
 
 
-def based_floor(x, base=10):
+def based_floor(x, base=1):
     """
-    rounds down to the last integer of base 10 (or other)
+    rounds down to the last multiple of base 10 (or other)
     """
-    return np.int(base * np.floor(np.float(x) / base))
+    return base * np.floor(np.float(x) / base)
 
 
-def based_ceil(x, base=10):
+def based_ceil(x, base=1):
     """
-    rounds up to the next integer of base 10 (or other)
+    rounds up to the next multiple of base 10 (or other)
     """
-    return np.int(base * np.ceil(np.float(x) / base))
+    return base * np.ceil(np.float(x) / base)
 
 
 def float(x):
